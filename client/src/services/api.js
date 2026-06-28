9efd17ed028a2e7edd6922b699be4f4f68f64556
Aditya99977 <Adiofficial8989gmail.com>
2026-06-28 13:41:17 +0530

    Update API URL for production deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://tasktracker-backend-yn01.onrender.com/api",
 });
 
 export default api;
